c3f7647a32244b1a0232b6a6b19a0d63cc1a018e
Godwin Idemudia <godwinide@gmail.com>
2024-11-05 17:14:05 +0100

    new

--- src/app/page.js
+++ src/app/page.js
@@ -259,17 +259,20 @@ const Home = () => {
         )
       }
 
-      <div className='mt-10'>
-        <div className="container text-base text-black flex items-center gap-4 bg-gray-50">
-          <div className="text-base font-bold text-gray-800">Token Balance:</div>
-          <div className='text-base font-bold text-gray-800'>{tokenBalance}</div>
+      {
+        connected
+        && <div className='mt-2'>
+          <div className="container text-base text-black flex items-center gap-4 bg-gray-50">
+            <div className="text-base font-bold text-gray-800">Token Balance:</div>
+            <div className='text-base font-bold text-gray-800'>{tokenBalance}</div>
+          </div>
         </div>
-      </div>
+      }
 
       {/* main start */}
       <div className='container flex flex-col gap-20 md:gap-10 md:flex-row-reverse'>
         {/* candidates images start */}
-        <div className='w-full md:w-3/5 relative'>
+        <div className='w-full md:w-2/5 relative'>
           <div className='w-full'>
             {/* progress */}
             <div className='flex justify-between mb-2'>
@@ -298,21 +301,36 @@ const Home = () => {
           </div>
           {/* vote count end */}
           {/* action buttons start */}
-          <div className='flex flex-row justify-around relative z-30'>
-            <button
-              onClick={() => handleVote("TRUMP")}
-              className={`cursor-pointer text-xs trump-bg text-white font-bold px-5 md:px-10 py-2 w-2/5 shadow-md rounded-sm`}
-            >{voting ? "Please wait..." : "VOTE TRUMP"}</button>
-            <button
-              onClick={() => handleVote("KAMALA")}
-              className={`cursor-pointer text-xs kamala-bg text-white font-bold px-5 md:px-10 py-2 w-2/5 shadow-md rounded-sm`}
-            >{voting ? "Please wait..." : "VOTE KAMALA"}</button>
-          </div>
+          {
+            connected && (Number(tokenBalance?.replace(/,/g, ""))) > 0
+            &&
+            <div className='flex flex-row justify-around relative z-30'>
+              <button
+                onClick={() => handleVote("TRUMP")}
+                className={`cursor-pointer text-xs trump-bg text-white font-bold px-5 md:px-10 py-2 w-2/5 shadow-md rounded-sm`}
+              >{voting ? "Please wait..." : "VOTE TRUMP"}</button>
+              <button
+                onClick={() => handleVote("KAMALA")}
+                className={`cursor-pointer text-xs kamala-bg text-white font-bold px-5 md:px-10 py-2 w-2/5 shadow-md rounded-sm`}
+              >{voting ? "Please wait..." : "VOTE KAMALA"}</button>
+            </div>
+          }
           {/* action buttons end */}
+          {connected && (Number(tokenBalance?.replace(/,/g, ""))) < 0
+            &&
+            <div className='w-full bg-gray-300 p-2 rounded mx-auto relative z-40'>
+              <div className='text-center text-black text-lg'>You need to hold $vote to vote</div>
+              <div className="my-2 divider"></div>
+              <div className='flex justify-between items-center gap-10'>
+                <a href='/' className='px-4 py-2 bg-sky-500 text-lg text-white rounded'>Buy on BullX</a>
+                <a href='/' className='px-4 py-2 bg-sky-500 text-lg text-white rounded'>Buy on Pumpfun</a>
+              </div>
+            </div>
+          }
         </div>
         {/* candidates images end */}
         {/* updates start */}
-        <div className='w-full md:w-2/5 px-4'>
+        <div className='w-full md:w-3/5 px-4'>
           <div className='text-black text-md font-bold md:font-medium uppercase'>Live Updates</div>
           <div className='divider'></div>
           {/* updates */}
